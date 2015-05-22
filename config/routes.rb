97680b1343a03c9d67aceb92b102a0866b4d4724
Daniel Dizzy <dizzylol4@gmail.com>
2015-05-22 16:26:48 +0900

    Made test page with task panel

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
     member do 
       put :change
     end
-    end
-
+  end
 
 
   devise_for :users
   root 'pages#home'
   get 'about' => 'pages#about'
+  get 'test' => 'pages#test'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
